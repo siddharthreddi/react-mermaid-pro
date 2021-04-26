c9d147b084690fd353c16e760b39bb94bd666db9
Siddharth Reddy <siddharth28b@gmail.com>
2021-04-26 09:30:58 +0530

    Updates

---
+++ precache-manifest.a093cf2844f165febc8818693d189292.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a18788d35feb202d299db1d71693e0d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5dd060b76c7ae9a9440b",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "48f648905fe4fbe807a1",
+    "url": "./static/js/2.b4afd859.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b4afd859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd060b76c7ae9a9440b",
+    "url": "./static/js/main.20afcb7b.chunk.js"
+  },
+  {
+    "revision": "b570c5aacaaea248e93c",
+    "url": "./static/js/runtime-main.615e073f.js"
+  }
+]);
